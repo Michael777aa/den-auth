42b14dd3a3b612d3511e1519e25438f171194fed
Mike <mikemike@Mikeui-MacBookPro.local>
2025-05-26 13:18:35 +0900

    feat:develop

--- dist/modules/Member/member.schema.d.ts
+++ dist/modules/Member/member.schema.d.ts
@@ -1,1 +1,73 @@
-export declare const memberModel: any;
+import mongoose from "mongoose";
+import { MemberProvider } from "../../libs/enums/member.enum";
+export declare const memberModel: mongoose.Model<{
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    email: string;
+    name: string;
+    sub: string;
+    provider: MemberProvider;
+    exp: number;
+    picture?: string | null | undefined;
+}, {}, {}, {}, mongoose.Document<unknown, {}, {
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    email: string;
+    name: string;
+    sub: string;
+    provider: MemberProvider;
+    exp: number;
+    picture?: string | null | undefined;
+}, {}> & {
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    email: string;
+    name: string;
+    sub: string;
+    provider: MemberProvider;
+    exp: number;
+    picture?: string | null | undefined;
+} & {
+    _id: mongoose.Types.ObjectId;
+} & {
+    __v: number;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any>, {}, {}, {}, {}, {
+    timestamps: true;
+}, {
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    email: string;
+    name: string;
+    sub: string;
+    provider: MemberProvider;
+    exp: number;
+    picture?: string | null | undefined;
+}, mongoose.Document<unknown, {}, mongoose.FlatRecord<{
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    email: string;
+    name: string;
+    sub: string;
+    provider: MemberProvider;
+    exp: number;
+    picture?: string | null | undefined;
+}>, {}> & mongoose.FlatRecord<{
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    email: string;
+    name: string;
+    sub: string;
+    provider: MemberProvider;
+    exp: number;
+    picture?: string | null | undefined;
+}> & {
+    _id: mongoose.Types.ObjectId;
+} & {
+    __v: number;
+}>>;
--- dist/modules/Member/member.service.d.ts
+++ dist/modules/Member/member.service.d.ts
@@ -4,5 +4,29 @@ export declare class MemberService {
      * Find a user by provider and sub, or create one if not exists.
      * 소셜 제공자와 sub로 사용자를 찾고, 없으면 새로 생성합니다.
      */
-    findOrCreateSocialMember(user: AuthUser): Promise<any>;
+    findOrCreateSocialMember(user: AuthUser): Promise<import("mongoose").Document<unknown, {}, {
+        createdAt: NativeDate;
+        updatedAt: NativeDate;
+    } & {
+        email: string;
+        name: string;
+        sub: string;
+        provider: import("../../libs/enums/member.enum").MemberProvider;
+        exp: number;
+        picture?: string | null | undefined;
+    }, {}> & {
+        createdAt: NativeDate;
+        updatedAt: NativeDate;
+    } & {
+        email: string;
+        name: string;
+        sub: string;
+        provider: import("../../libs/enums/member.enum").MemberProvider;
+        exp: number;
+        picture?: string | null | undefined;
+    } & {
+        _id: import("mongoose").Types.ObjectId;
+    } & {
+        __v: number;
+    }>;
 }
