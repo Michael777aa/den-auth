233fd6ecd7fe4435fc3a0b9351aa67200a764fda
Mike <mikemike@Mikeui-MacBookPro.local>
2025-05-27 19:10:36 +0900

    feat:develop

--- deploy.sh
+++ deploy.sh
@@ -12,21 +12,12 @@ npm install
 
 echo "Building the application..."
 npm run build
-npm run start
 
 echo "Starting the application in production mode using PM2..."
 pm2 start dist/server.js --name=car-washing-service --env production
 
+# Start ngrok with PM2 on the desired port (change 3110 to your Fastify port)
+echo "Starting ngrok tunnel using PM2..."
+pm2 start "ngrok http 3112 --log=stdout" --name=ngrok-tunnel
+
 echo "Production deployment complete!"
-
-# DEVELOPMENT (optional)
-# echo "Switching to the development branch and pulling the latest changes from Git..."
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# echo "Installing dependencies..."
-# npm install
-
-# echo "Starting the application in development mode..."
-# pm2 start "npm run start:prod" --name=car-washing-service-dev
