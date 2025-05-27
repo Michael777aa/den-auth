f2917cd2718c35003cd10e9ca35676741ae6d947
Mike <mikemike@Mikeui-MacBookPro.local>
2025-05-27 18:10:14 +0900

    feat:develop

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({
-  path: process.env.NODE_ENV === "production" ? ".env" : ".env",
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
 });
 import mongoose from "mongoose";
 import initApp from "./app";
